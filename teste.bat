71042a6baf917262e58b0e81f9b418784f30c50f
wagnerBR <wagner.b.rodrigues@gmail.com>
2023-09-10 14:02:26 -0300

    Ajustes requirements,txt

--- teste.bat
+++
@@ -1,8 +0,0 @@
-REM Ler as configurações do MySQL do arquivo mysql_config.conf dentro da subpasta db
-for /f "delims=" %%a in (db\mysql_config.conf) do set "%%a"
-
-REM Restaurar o banco de dados no MySQL
-echo %container_name%  
-echo %host% 
-echo %password% %database% 
-echo %backup_file%
