4af45550ce5e7734eb7afcc99075f07f7e641571
wagnerbrodrigues <wagner.b.rodrigues@gmail.com>
2024-02-26 15:59:06 -0300

    alterado o valor Bazin e Dump de Dados.

--- app/acoes/processa_fundamentalista.py
+++ app/acoes/processa_fundamentalista.py
@@ -98,7 +98,7 @@ class fundamentalista:
     def calculo_Bazin(self, df, periodo):
         media = self.media_por_periodo(df, periodo)
         #soma_valor = df['valor'].sum()
-        return  (media * 100) / 6
+        return  (media * 100) / 7
 
     def calcular_gordon(self, df, taxa_retorno):
         if df.empty:
